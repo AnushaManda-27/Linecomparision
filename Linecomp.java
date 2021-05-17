e508ee811531ef7fa93aa98e69e7bff9c6e937c8
AnushaManda-27 <anushamanda27@gmail.com>
2021-05-17 18:25:09 +0530

    Added linecomparision with object

--- Linecomp.java
+++ Linecomp.java
@@ -1,31 +1,45 @@
 import static java.lang.Math.sqrt;
-public class Linecomp {
+ public class Linecomp {
+  private final int x1;
+  private final int x2;
+  private final int y1;
+  private final int y2;
+  private final int x3;
+  private final int x4;
+  private final int y3;
+  private final int y4;
 
-public static final int x1=3;
-public static final int x2=5;
-public static final int y1=3;
-public static final int y2=5;
-public static final int x3=3;
-public static final int x4=5;
-public static final int y3=3;
-public static final int y4=5;
+ public Linecomp(int x1, int x2, int y1, int y2, int x3, int y3, int x4, int y4) {
+  this.x1 = x1;
+  this.x2 = x2;
+  this.y1 = y1;
+  this.y2 = y2;
+  this.x3 = x3;
+  this.x4 = x4;
+  this.y3 = y3;
+  this.y4 = y4;
+ }
+ public void comparision(){
+ double l1 =Math.round( sqrt( ((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)) ));
+  System.out.println("l1:" +l1);
 
-public static void main(String[] args) {
+ double l2 =Math.round( sqrt( ((x4-x3) * (x4-x3)) + ((y4-y3) * (y4-y3)) ));
+  System.out.println("l2:" +l2);
 
-double l1 =Math.round( sqrt( ((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)) ));
- System.out.println("l1:" +l1);
+   if ( l1 == l2) {
+      System.out.println("l1 is equal to l2");
+   }
+   else if( l1 > l2) {
+      System.out.println("l1 is greater to l2");
+   }
+   else{
+      System.out.println("l1 is less than l2");
+   }
+ }
 
-double l2 =Math.round( sqrt( ((x4-x3) * (x4-x3)) + ((y4-y3) * (y4-y3)) ));
- System.out.println("l2:" +l2);
-
-  if ( l1 == l2) {
-     System.out.println("l1 is equal to l2");
-  }
-  else if( l1 > l2) {
-     System.out.println("l1 is greater to l2");
-  }
-  else{
-     System.out.println("l1 is less than l2");
-  }
+ public static void main (String[] args){
+   Linecomp firstvalue = new Linecomp(3,4,5,6,7,8,9,10);
+   firstvalue.comparision();
+   System.out.println(firstvalue);
  }
 }
---
+++ Linecomp1.java
@@ -0,0 +1,47 @@
+import static java.lang.Math.sqrt;
+public class Linecomp1 {
+
+private final int x1;
+private final int x2;
+private final int y1;
+private final int y2;
+private final int x3;
+private final int x4;
+private final int y3;
+private final int y4;
+
+public Linecomp1(int x1, int x2, int y1, int y2, int x3, int y3, int x4, int y4) {
+ this.x1 = x1;
+ this.x2 = x2;
+ this.y1 = y1;
+ this.y2 = y2;
+ this.x3 = x3;
+ this.x4 = x4;
+ this.y3 = y3;
+ this.y4 = y4;
+}
+public void comparision(){
+double l1 =Math.round( sqrt( ((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)) ));
+ System.out.println("l1:" +l1);
+
+double l2 =Math.round( sqrt( ((x4-x3) * (x4-x3)) + ((y4-y3) * (y4-y3)) ));
+ System.out.println("l2:" +l2);
+
+  if ( l1 == l2) {
+     System.out.println("l1 is equal to l2");
+  }
+  else if( l1 > l2) {
+     System.out.println("l1 is greater to l2");
+  }
+  else{
+     System.out.println("l1 is less than l2");
+  }
+}
+
+public static void main (String[] args){
+  Linecomp1 firstvalue = new Linecomp1(3,4,5,6,7,8,9,10);
+  firstvalue.comparision();
+  System.out.println(firstvalue);
+}
+}
+
